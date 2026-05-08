d9f735f9bafca1f7c4d44b53eb96a2ad61fc2b03
Mariana F <marianafama@unisabana.edu.co>
2026-05-07 21:13:40 -0500

    8

--- utils.py
+++ utils.py
@@ -10,4 +10,11 @@ def person_data():
     while not done:
         acc_num = int(input("Enter a 4-digit account number:\n"))
         balance = float(input("Enter the initial balance:\n"))
+        nueva_cuenta = BankAccount(acc_num, balance)
+        persona.add_account(nueva_cuenta)
         
+        respuesta = input("Are you done adding accounts? (yes/no):\n").lower()
+        if respuesta == "yes":
+            done = True
+            
+    return persona
