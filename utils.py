3df98bedc00e6f17cdb2654aa4a865401ff96856
Mariana F <marianafama@unisabana.edu.co>
2026-05-07 21:10:28 -0500

    7

--- utils.py
+++ utils.py
@@ -1,1 +1,13 @@
 # utils.py
+from person import Person
+from bank_account import BankAccount
+
+def person_data():
+    name = input("Enter the person's name:\n")
+    persona = Person(name)
+    
+    done = False
+    while not done:
+        acc_num = int(input("Enter a 4-digit account number:\n"))
+        balance = float(input("Enter the initial balance:\n"))
+        
