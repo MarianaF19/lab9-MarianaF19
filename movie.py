976fdf0f41f423c5065ce3a043adf06681da330f
Mariana F <marianafama@unisabana.edu.co>
2026-05-07 21:30:23 -0500

    18

--- movie.py
+++ movie.py
@@ -6,7 +6,9 @@ class Movie:
         self.director = director
         self.year = year
 # FREEZE CODE END
-    # TODO: Define the __str__ method!
+    
+    def _str_(self):
+        return f"Movie: {self.title} (Directed by {self.director}, {self.year})"
 
 
 # FREEZE CODE BEGIN
@@ -17,6 +19,5 @@ if __name__ == "__main__":
     year = input("Enter the release year: ")
 # FREEZE CODE END
     
-    
-    # TODO: Construct a Movie object!
-    # TODO: Print the object!
+    mi_pelicula = Movie(title, director, year)
+    print(mi_pelicula)
