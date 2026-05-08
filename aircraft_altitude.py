25f3170e7e7aa9c0515f4d5bad49f47d0d70f0bc
Mariana F <marianafama@unisabana.edu.co>
2026-05-07 20:58:08 -0500

    2

--- aircraft_altitude.py
+++ aircraft_altitude.py
@@ -3,3 +3,9 @@ from aircraft import Aircraft
 def main():
     model = input("Enter aircraft model: ")
     my_aircraft = Aircraft(model)
+    while True:
+        user_input = input("Enter command (A for ascent, D for descent, X to exit): ")
+        
+        parts = user_input.split()
+        command = parts[0].upper() 
+        
