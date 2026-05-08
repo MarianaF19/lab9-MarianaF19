41261229a9f42297d476528e597fd4f4d921b04a
Mariana F <marianafama@unisabana.edu.co>
2026-05-07 21:19:00 -0500

    12

--- person.py
+++ person.py
@@ -1,1 +1,12 @@
 # person.py
+
+class Person:
+    def _init_(self, name):
+        self.name = name
+        self.accounts = []
+
+    def add_account(self, account):
+        self.accounts.append(account)
+
+    def _str_(self):
+        return f"Name = {self.name}, Number of accounts = {len(self.accounts)}"
