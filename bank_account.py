9df4c73ad00d7f48c8529f06d4a066d92d79c544
Mariana F <marianafama@unisabana.edu.co>
2026-05-10 20:59:41 -0500

    28

--- bank_account.py
+++ bank_account.py
@@ -1,26 +1,22 @@
 # bank_account.py
 
 class BankAccount:
-    def _init_(self, account_number, balance=0.0):
-        
-        self.account_number = account_number
-        self.balance = float(balance)
+    
+        def _init_(self, account_number, balance=0.0):
+            self.account_number = account_number
+            self.balance = float(balance)
 
-    def deposit(self, amount):
-        
-        self.balance += amount
+        def deposit(self, amount):
+            self.balance += amount
 
-    def withdraw(self, amount):
-        
-        if amount > self.balance:
-            return -1
-        else:
-            self.balance -= amount
-            return 0
+        def withdraw(self, amount):
+            if amount > self.balance:
+                return -1  # Fondos insuficientes
+            else:
+                self.balance -= amount
+            return 0   # Éxito
 
-    def _str_(self):
-        acc_str = str(self.account_number)
-        last_two = acc_str[-2:]
-        
-        return (f"Account Number: **{last_two}\n"
-                f"Current Balance: {self.balance:.1f}")
+        def _str_(self):
+        # Convertimos a string para tomar los últimos dos caracteres
+            last_two = str(self.account_number)[-2:]
+            return f"Account Number: **{last_two}\nCurrent Balance: {self.balance:.2f}"
