f11c972b397eea87fc37692b4571e5ba7688bd80
Mariana F <marianafama@unisabana.edu.co>
2026-05-11 11:12:17 -0500

    44

--- song.py
+++ song.py
@@ -1,12 +1,12 @@
 # Write your code here!
 class Song:
-    def _init_(self, name, artist, length):
+    def __init__(self, name, artist, length):
         self.name = name
         self.artist = artist
         self.length = length
 
     def get_length_in_seconds(self):
-        return self.length * 60
+        return self.length * 60.0
     
     def _str_(self):
         return f"'{self.name}' by {self.artist} ({self.length})"
