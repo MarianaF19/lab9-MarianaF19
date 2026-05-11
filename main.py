717e49302753fe256f80e314d9976b91650580c3
Mariana F <marianafama@unisabana.edu.co>
2026-05-10 21:06:33 -0500

    29

--- main.py
+++ main.py
@@ -1,56 +1,44 @@
-from bank_account import BankAccount
 from person import Person
-from utils import person_data, balance_summary
+from bank_account import BankAccount
+import utils
 
 def main():
-    people = []  # List to store all Person objects
+    people = []
 
     while True:
-        # Display menu
-        print("Choose an option:")
+        print("\nChoose an option:")
         print("1. Add a new person")
         print("2. Add an account to a person")
         print("3. Show all balances")
         print("4. Quit")
+        
+        option = input()
 
-        choice = input().strip()
+        if option == "1":
+            new_person = utils.person_data()
+            people.append(new_person)
 
-        # Option 1: Add a new person
-        if choice == "1":
-            nueva_persona = person_data()
-            people.append(nueva_persona)
-
-        # Option 2: Add an account to an existing person
-        elif choice == "2":
-            name_to_find = input("Enter the person's name:\n")
-            found_person = None
-            for p in people:
-                if p.name == name_to_find:
-                    found_person = p
-                    break
+        elif option == "2":
+            target_name = input("Enter the person's name:\n")
+            # Buscar a la persona en la lista
+            found_person = next((p for p in people if p.name == target_name), None)
             
             if found_person:
                 acc_num = int(input("Enter a 4-digit account number:\n"))
                 balance = float(input("Enter the initial balance:\n"))
-                
-                new_acc = BankAccount(acc_num, balance)
-                found_person.add_account(new_acc)
+                found_person.add_account(BankAccount(acc_num, balance))
             else:
                 print("Person not found.")
 
-        # Option 3: Show all balances
-        elif choice == "3":
+        elif option == "3":
             if not people:
                 print("No data to show.")
             else:
-                balance_summary(people)
+                utils.balance_summary(people)
 
-        # Option 4: Quit
-        elif choice == "4":
+        elif option == "4":
             print("Goodbye!")
             break
-
-        # Invalid input
         else:
             print("Invalid option. Please choose 1-4.")
 
